e2d76366846925178d35ba932e9e15f5dc1ea6fa
Yutuc <psyutuc@hotmail.com>
2019-09-13 22:01:21 -0500

    - Check in notification added

--- app/src/main/java/com/univation/tdsadmin/ChooseUserActivity.kt
+++ app/src/main/java/com/univation/tdsadmin/ChooseUserActivity.kt
@@ -1,6 +1,14 @@
 package com.univation.tdsadmin
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
+import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.DividerItemDecoration
@@ -8,12 +16,11 @@ import android.support.v7.widget.SearchView
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.EditText
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.ChildEventListener
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.*
 import com.univation.tdsadmin.adapters.UserRow
+import com.univation.tdsadmin.objects.CheckInObject
 import com.univation.tdsadmin.objects.UserObject
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.ViewHolder
@@ -28,11 +35,20 @@ class ChooseUserActivity : AppCompatActivity() {
         var userChosen: UserObject? = null
     }
 
+    lateinit var notificationManager : NotificationManager
+    lateinit var notificationChannel : NotificationChannel
+    lateinit var builder : Notification.Builder
+
+    private val channelId = "com.umbrellaman.tdsadmin"
+    private val description = "TDSAdmin"
+    private val notificationID = 1999
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_choose_user)
         setTitle("Select user")
         verifyUserIsLoggedIn()
+        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
         adapter.setOnItemClickListener { item, _ ->
             val userClicked = item as UserRow
@@ -44,6 +60,7 @@ class ChooseUserActivity : AppCompatActivity() {
         recyclerview_choose_user.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
         recyclerview_choose_user.adapter = adapter
 
+        checkInNotification()
         pullUsers()
     }
 
@@ -54,6 +71,77 @@ class ChooseUserActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }//verifyUserIsLoggedIn method
+
+    private fun checkInNotification(){
+        val ref = FirebaseDatabase.getInstance().getReference("/check-ins")
+        ref.addChildEventListener(object: ChildEventListener{
+            override fun onCancelled(p0: DatabaseError) {
+
+            }
+
+            override fun onChildMoved(p0: DataSnapshot, p1: String?) {
+
+            }
+
+            override fun onChildChanged(p0: DataSnapshot, p1: String?) {
+                pullUserName(p0.key!!)
+            }
+
+            override fun onChildAdded(p0: DataSnapshot, p1: String?) {
+
+            }
+
+            override fun onChildRemoved(p0: DataSnapshot) {
+
+            }
+
+        })
+    }//checkInNotification function
+
+    private fun showNotification(userName: String){
+        val intent = Intent(this, ChooseUserActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(this,0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            notificationChannel = NotificationChannel(channelId, description, NotificationManager.IMPORTANCE_HIGH)
+            notificationChannel.enableLights(true)
+            notificationChannel.lightColor = Color.GREEN
+            notificationChannel.enableVibration(false)
+            notificationManager.createNotificationChannel(notificationChannel)
+
+            builder = Notification.Builder(this, channelId)
+                .setContentTitle("$userName Checked-In!")
+                .setAutoCancel(true)
+                .setPriority(Notification.PRIORITY_DEFAULT)
+                .setSmallIcon(R.drawable.abc_ic_star_black_48dp)
+                .setLargeIcon(BitmapFactory.decodeResource(this.resources, R.mipmap.ic_launcher_foreground))
+                .setContentIntent(pendingIntent)
+        }
+        else {
+            builder = Notification.Builder(this)
+                .setContentTitle("$userName Checked-In!")
+                .setAutoCancel(true)
+                .setPriority(Notification.PRIORITY_DEFAULT)
+                .setSmallIcon(R.drawable.abc_ic_star_black_48dp)
+                .setLargeIcon(BitmapFactory.decodeResource(this.resources, R.mipmap.ic_launcher_foreground))
+                .setContentIntent(pendingIntent)
+        }
+        notificationManager.notify(notificationID, builder.build())
+    }
+
+    private fun pullUserName(uid: String){
+        val ref = FirebaseDatabase.getInstance().getReference("/users").child("$uid")
+        ref.addValueEventListener(object: ValueEventListener{
+            override fun onCancelled(p0: DatabaseError) {
+
+            }
+
+            override fun onDataChange(p0: DataSnapshot) {
+                val userObject = p0.getValue(UserObject::class.java)!!
+                showNotification("${userObject.firstName} ${userObject.lastName}")
+            }
+        })
+    }
 
     private fun pullUsers(){
         val ref = FirebaseDatabase.getInstance().getReference("/users")
